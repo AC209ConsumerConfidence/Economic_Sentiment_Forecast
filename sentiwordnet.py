47b781fa40a6e108561921c2f2b9d8541b5cbdf2
Mali Akmanalp <mali@akmanalp.com>
2016-12-14 12:35:11 -0500

    Add version where we just sum scores.

--- sentiwordnet.py
+++ sentiwordnet.py
@@ -52,3 +52,33 @@ def get_sentiment(article):
         return 0, 0, 0
 
     return np.average(pos_scores, weights=subj_scores), np.average(neg_scores, weights=subj_scores), np.mean(subj_scores)
+
+
+def get_sentiment_2(article):
+    """Returns: single sentiment score, word count."""
+
+    sentences = nltk.sent_tokenize(article)
+    sentence_words = [nltk.word_tokenize(sentence) for sentence in sentences]
+    tagged_sentence_words = flatten(nltk.pos_tag_sents(sentence_words))
+
+    # Filter stopwords
+    tagged_sentence_words = [word for word in tagged_sentence_words if word[1] not in english_stopwords]
+
+    scores = []
+    for word, pos in tagged_sentence_words:
+
+        swn_pos = nltk_to_sentiwordnet.get(pos[:2], None)
+
+        if swn_pos is None:
+            continue
+
+        synsets = swn.senti_synsets(word.lower(), pos=swn_pos)
+
+        if len(synsets) == 0:
+            continue
+
+        # Get scores from the first synonym
+        score = synsets[0].pos_score() - synsets[0].neg_score()
+        scores.append(score)
+
+    return np.sum(scores), len(scores)
